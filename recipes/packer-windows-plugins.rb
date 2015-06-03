a87dcbe3bb2f88d62c04cb96472624cc3cfa399a
rich-h-r <richard.h.rees@gmail.com>
2015-06-03 14:31:28 +0100

    just want to dump the files

--- recipes/packer-windows-plugins.rb
+++ recipes/packer-windows-plugins.rb
@@ -3,5 +3,5 @@ include_recipe "ark"
 ark 'packer-windows-plugins' do
   url "#{node[:packer_windows_plugin][:url_base]}/v#{node[:packer_windows_plugin][:version]}/#{node[:os]}_#{node[:packer][:arch]}.zip"
   path "/usr/local/packer-#{node[:packer][:version]}"
-  action :put
+  action :dump
 end
