63ff62a83f7948561f596e0092890b62b9f135b9
rich-h-r <richard.h.rees@gmail.com>
2015-06-03 14:14:22 +0100

    unpack to correct directory

--- recipes/packer-windows-plugins.rb
+++ recipes/packer-windows-plugins.rb
@@ -1,6 +1,7 @@
 include_recipe "ark"
 
-ark 'packer' do
+ark 'packer-windows-plugins' do
   url "#{node[:packer_windows_plugin][:url_base]}/v#{node[:packer_windows_plugin][:version]}/#{node[:os]}_#{node[:packer][:arch]}.zip"
+  path "/usr/local/packer-#{node[:packer][:version]}"
   action :put
 end
