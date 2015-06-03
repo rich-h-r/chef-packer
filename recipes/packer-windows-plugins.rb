3d92bb78a9ea49237797d5b6520042771536cf4b
rich-h-r <richard.h.rees@gmail.com>
2015-06-03 15:22:21 +0100

    unpack to correct directory

--- recipes/packer-windows-plugins.rb
+++ recipes/packer-windows-plugins.rb
@@ -1,7 +1,9 @@
 include_recipe "ark"
 
-ark 'packer-windows-plugins' do
+ark 'packer' do
   url "#{node[:packer_windows_plugin][:url_base]}/v#{node[:packer_windows_plugin][:version]}/#{node[:os]}_#{node[:packer][:arch]}.zip"
-  path "/usr/local/packer-#{node[:packer][:version]}"
+  version "#{node[:packer_windows_plugin][:version]}"
+  path "/usr/local"
+  creates "packer-communicator-winrm"
   action :dump
 end
